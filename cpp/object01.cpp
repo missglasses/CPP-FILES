6b9f9a9ab62711d78ed9580c5de1d25c2e572628
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-08-10 14:43:02 +0800

    Create object01.cpp

---
+++ cpp/object01.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Animal {
+   public: 
+   string species; //property
+   int age; //property
+   int name; //property
+   
+   //functions
+   void eat(){
+       
+   }
+   
+   void sleep(){
+       
+   }
+   
+   void  makeSound(){
+       
+   }
+    
+};
+
+int main (){
+    
+    Animal cat; //<--this is the object
+    
+    return 0;
+}
