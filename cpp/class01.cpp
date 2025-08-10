c3c5b21b03373a3a304f174984cee313c606f762
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-08-10 14:41:23 +0800

    Create class01.cpp

---
+++ cpp/class01.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class Animal {
+   public: 
+   string species; //property
+   int age; //property
+   int name; //property
+   
+   //functions
+   void eat(){
+       
+   }
+   
+   void sleep(){
+       
+   }
+   
+   void  makeSound(){
+       
+   }
+    
+    
+};
+
+
+int main (){
+
+
+
+}
